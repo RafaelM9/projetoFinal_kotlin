06c481a87acfaafd3bc330b01eace7dd03e002e9
rafae <rafaelmorais252002@gmail.com>
2025-05-13 15:27:12 +0100

    declaração de variaveis com estado remember e implementação do algoritmo de conversão de fahrenheit para celsius

--- app/src/main/java/pt/ipg/converterfahrenheitcelsius/MainActivity.kt
+++ app/src/main/java/pt/ipg/converterfahrenheitcelsius/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pt.ipg.converterfahrenheitcelsius.ui.theme.ConverterFahrenheitCelsiusTheme
@@ -19,15 +25,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConverterFahrenheitCelsiusTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    ConverterFahrenheitCelsius()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ConverterFahrenheitCelsius(){
+
+    var fahrenheitInput by remember { mutableStateOf("") }
+
+    val fahrenheit = fahrenheitInput.toDoubleOrNull()
+    val celsius = fahrenheit?.let { (it - 32) * 5 / 9 }
+
+
 }
 
 @Composable
@@ -40,8 +54,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TemperatureConverterPreview() {
     ConverterFahrenheitCelsiusTheme {
-        Greeting("Android")
+        ConverterFahrenheitCelsius()
     }
 }
