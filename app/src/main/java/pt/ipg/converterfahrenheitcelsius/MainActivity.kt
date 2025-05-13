9d30a5c1f8bd9f4192a69d853cc5d256d35d88cf
rafae <rafaelmorais252002@gmail.com>
2025-05-13 15:27:12 +0100

    declaração de variaveis com estado remember e implementação do algoritmo de conversão de fahrenheit para celsius

--- app/src/main/java/pt/ipg/converterfahrenheitcelsius/MainActivity.kt
+++ app/src/main/java/pt/ipg/converterfahrenheitcelsius/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pt.ipg.converterfahrenheitcelsius.ui.theme.ConverterFahrenheitCelsiusTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +29,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConverterFahrenheitCelsiusTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    ConverterFahrenheitCelsius()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ConverterFahrenheitCelsius(){
+
+    var fahrenheitInput by remember { mutableStateOf("") }
+
+    val fahrenheit = fahrenheitInput.toDoubleOrNull()
+    val celsius = fahrenheit?.let { (it - 32) * 5 / 9 }
+
+    Column(
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = fahrenheitInput,
+            onValueChange = {fahrenheitInput = it},
+            label = { Text("Temperatura em Fahrenheit") }
+        )
     }
 }
 
@@ -40,8 +70,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TemperatureConverterPreview() {
     ConverterFahrenheitCelsiusTheme {
-        Greeting("Android")
+        ConverterFahrenheitCelsius()
     }
 }
