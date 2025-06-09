b8f3c53de87d9b8450e86cf885e956198c959938
rafae <rafaelmorais252002@gmail.com>
2025-06-09 22:54:01 +0100

    Adicionado o layout basico de IU com coluna e título

--- app/src/main/java/pt/ipg/conversorcelsiusfahrenheit/MainActivity.kt
+++ app/src/main/java/pt/ipg/conversorcelsiusfahrenheit/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pt.ipg.conversorcelsiusfahrenheit.ui.theme.ConversorCelsiusFahrenheitTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +26,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConversorCelsiusFahrenheitTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color(0xFFBBDEFB)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(32.dp)
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Selecionar tipo de conversão",
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        }
+                    }
+
             }
         }
     }
