b5b2667b5844abf901b7a333a8005ea90444c8b6
rafae <rafaelmorais252002@gmail.com>
2025-06-13 13:00:49 +0100

    adicionar funcionalidade de um teclado numerico para intruduzir valorees

--- app/src/main/java/pt/ipg/conversorcelsiusfahrenheit/MainActivity.kt
+++ app/src/main/java/pt/ipg/conversorcelsiusfahrenheit/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -111,6 +112,8 @@ fun ConverterCelsiusFahrenheit() {
                     }
                 )
             },
+            // adiciona teclado numerico para introduzir numeros
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
             modifier = Modifier.fillMaxWidth()
         )
